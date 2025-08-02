61458af617ffe2eca4aa1cad48b8e13da2cf546b
SayedThoha <147703696+SayedThoha@users.noreply.github.com>
2025-08-02 23:28:20 +0530

    updated the user file

--- src/app/pipes/filter-pipe.pipe.ts
+++ src/app/pipes/filter-pipe.pipe.ts
@@ -14,10 +14,14 @@ export class FilterPipePipe implements PipeTransform {
     let searchName = nameSearch.trim().toLowerCase();
     let searchCity = citySearch.trim().toLowerCase();
     let searchCompany = companySearch.trim().toLowerCase();
-    
+
     return value.filter((val) => {
-      let name = nameSearch ? val.name.toLowerCase().includes(searchName) : true;
-      let city = citySearch ? val.address.city.toLowerCase().includes(searchCity) : true;
+      let name = nameSearch
+        ? val.name.toLowerCase().includes(searchName)
+        : true;
+      let city = citySearch
+        ? val.address.city.toLowerCase().includes(searchCity)
+        : true;
       let company = companySearch
         ? val.company.name.toLowerCase().includes(searchCompany)
         : true;
